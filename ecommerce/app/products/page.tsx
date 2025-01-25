b4344442c4c024eb85275d8dbec610b1374309ff
Spartak-Belov-Floresku <spartakbelovfloresku@gmail.com>
2025-01-25 16:51:21 -0500

    added navigation bar

--- ecommerce/app/products/page.tsx
+++ ecommerce/app/products/page.tsx
@@ -3,10 +3,10 @@ import { products } from "../product-data";
 
 const ProductsPage = () => {
     return (
-        <>
-            <h1>Products</h1>
+        <div className="container mx-auto p-8">
+            <h1 className="text-4xl font-bold mb-8">Products</h1>
             <ProductsList products={products} />
-        </>
+        </div>
     )
 }
 
